5e074785da94b445689e5817120694527f6dbe70
Lukas <babecka@outlook.com>
2025-02-19 13:34:50 +0100

    update bike return string based on horsepower

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -9,18 +9,31 @@ public class Vehicle {
     // false = unoccupied, true = occupied
     private boolean[] seats;
 
-    public Vehicle(Type type, int maxSeats) {
+    private int horsepower;
+
+    public Vehicle(Type type, int maxSeats, int horsepower) {
         this.type = type;
 
         this.seats = new boolean[maxSeats];
         for (int i = 0; i < maxSeats; i++) {
             this.seats[i] = false;
         }
+
+        this.horsepower = horsepower;
     }
 
     public String GetVehicleType() {
         switch (this.type) {
-            case BIKE: return "Bike";
+            case BIKE: {
+                if (this.horsepower <= 15)
+                    return "Scooter";
+                else if (this.horsepower > 15 && this.horsepower <= 50)
+                    return "Cruiser";
+                else if (this.horsepower > 50 && this.horsepower <= 150)
+                    return "Sports Bike";
+                else
+                    return "Superbike";
+            }
             case CAR: return "Car";
             default: return "None";
         }
