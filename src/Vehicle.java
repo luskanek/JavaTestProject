c619535844263a9c98af6c4b0083583a3575cdea
Lukas <babecka@outlook.com>
2025-02-19 13:13:25 +0100

    implement Vehicle types

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,3 +1,20 @@
 public class Vehicle {
+    enum Type {
+        BIKE,
+        CAR,
+    }
 
+    private Type type;
+
+    public Vehicle(Type type) {
+        this.type = type;
+    }
+
+    public String GetVehicleType() {
+        switch (this.type) {
+            case BIKE: return "Bike";
+            case CAR: return "Car";
+            default: return "None";
+        }
+    }
 }
