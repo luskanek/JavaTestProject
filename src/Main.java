bd68d0d134797cb6fddb796181e6a6689a296f57
Lukas <babecka@outlook.com>
2025-03-20 13:56:37 +0100

    add test inner class

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,15 @@
 public class Main {
+    class NestedClass {
+        public static String ReturnText(boolean condition) {
+            if (condition == true) {
+                return "";
+            }
+            else {
+                return "Text";
+            }
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
